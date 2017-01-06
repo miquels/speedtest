041bfa6b8b714edcb5cda10111e02901a2583b53
Miquel van Smoorenburg <mike@langeraar.net>
2017-01-06 14:00:44 +0100

    update collapse directive

--- src/directives/Collapse.js
+++ src/directives/Collapse.js
@@ -1,16 +1,61 @@
 
-function toggle (ev) {
-  let nodes = ev.target.parentNode.getElementsByClassName('collapse')
-  for (let node = 0; node < nodes.length; node++) {
-    nodes[node].classList.toggle('in')
+let onList = []
+
+let lastClick = {
+  target: null,
+  timeStamp: null
+}
+
+function toggle (node, className, on) {
+  let nodes = node.getElementsByClassName('collapse')
+  if (on === undefined || on === null) {
+    on = true
+    for (let i = 0; i < nodes.length; i++) {
+      if (nodes[i].classList.contains('in')) {
+        on = false
+      }
+    }
+  }
+  if (on) {
+    for (let i = 0; i < nodes.length; i++) {
+      nodes[i].classList.add('in')
+    }
+    onList.push({ node: node, className: className })
+  } else {
+    for (let i = 0; i < nodes.length; i++) {
+      nodes[i].classList.remove('in')
+    }
+    let i = onList.findIndex((el) => el.className === className && el.node === node)
+    if (i >= 0) {
+      onList.splice(i, 1)
+    }
+  }
+}
+
+function clickElem (ev) {
+  lastClick.target = ev.target
+  lastClick.timeStamp = ev.timeStamp
+  let node = ev.target.parentNode
+  toggle(node, 'collapse')
+}
+
+function clickWindow (ev) {
+  if (ev.target === lastClick.target && ev.timeStamp === lastClick.timeStamp) {
+    return
+  }
+  if (onList.length > 0) {
+    var l = onList.pop()
+    toggle(l.node, l.className, false)
   }
 }
 
 export default {
   bind (el, binding) {
-    el.addEventListener('click', toggle)
+    el.addEventListener('click', clickElem)
+    window.addEventListener('click', clickWindow)
   },
   unbind (el, binding) {
-    el.removeEventListener('click', toggle)
+    el.removeEventListener('click', clickElem)
+    window.removeEventListener('click', clickWindow)
   }
 }
